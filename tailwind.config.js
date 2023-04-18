a340d8bfc7ef7b861d0293d126414855e08d51ec
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-04-18 20:53:59 +0530

    made developer page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./root/**/*.{html,js}"],
+	content: [
+		"./root/**/*.{html,js}",
+		"./node_modules/tw-elements/dist/js/**/*.js",
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("tw-elements/dist/plugin.cjs")],
+	// darkMode: "class",
 };
